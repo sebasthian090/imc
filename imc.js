e9131d456aeb739b70644ca52a26c2be3a5e341c
sebasthian090 <163420495+sebasthian090@users.noreply.github.com>
2024-11-12 12:37:08 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -4,10 +4,21 @@ let altura = parseFloat(prompt("digite o valor da altura: "));
 if(isNaN(peso) || isNaN(altura)|| peso <=0 || altura<=0){
     document.write("digite valores válidos : ");
 }else{
-     let imc = peso/(altura*altura).toFixed(2);
-     document.write("seu imc é igual a:", imc);
+  let imc = (peso / (altura * altura)).toFixed(2);
+  document.write("Seu imc é igual a<span style='color: red;'>:", imc, "</span>");
+  document.write("<br>");
 
-     if(imc<=18){
-       document.write("seu imc está abaixo ") 
-     }
+  if (imc <= 18.5) {
+    document.write("Seu imc está abaixo do normal!!");
+  } else if (imc > 18.5 && imc <= 24.9) {
+    document.write("Seu imc está Normal!!");
+  } else if (imc > 25 && imc <= 29.9) {
+    document.write("Seu imc indica que você está em sobrepeso!!");
+  } else if (imc > 30 && imc <= 34.9) {
+    document.write("Seu imc indica que você está em Obesidade grau I!!");
+  }
 }
+     
+     
+     
+    
